56697f8c41e8c8477f6a590f3c6353c34dab29a2
Michael Kuehl <mickuehl@ratchet.cc>
2020-02-21 14:47:30 +0100

    clean-up before start of real implementations

--- internal/types/slack.go
+++ internal/types/slack.go
@@ -16,21 +16,3 @@ type (
 		Updated int64
 	}
 )
-
-/*
-
-{
-    "ok": true,
-    "access_token": "xoxb-941724765634-944826281235-n75RuOTOTYh3bXPl2Cbj3a7s",
-    "token_type": "bot",
-    "scope": "commands,incoming-webhook,team:read",
-    "app_id": "AU5V984BZ",
-    "bot_user_id": "UTSQA896X",
-    "incoming_webhook": {
-        "url": "https://hooks.slack.com/services/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",
-        "channel": "#general",
-        "configuration_url": "https://lnkk-ai-dev.slack.com/services/BUALCC6DN"
-    }
-}
-
-*/
--- pkg/api/types.go
+++ pkg/api/types.go
@@ -5,4 +5,8 @@ const (
 	FullName string = "lnkk.app"
 	// Version is the human readable version string of this build
 	Version string = "1.0"
+	// SchedulerBaseURL is the prefix for all scheduller/cron tasks
+	SchedulerBaseURL string = "/_i/1/scheduler"
+	// JobsBaseURL is the prefix for all scheduled jobs
+	JobsBaseURL string = "/_i/1/jobs"
 )
