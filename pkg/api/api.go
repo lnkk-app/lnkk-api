8501fbbd5c71626766ce4b170b5d747d0cfd5ebf
Michael Kuehl <mickuehl@ratchet.cc>
2020-06-18 08:19:26 +0200

    replaced code with similar function from the commons

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -7,9 +7,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 
+	"github.com/majordomusio/commons/pkg/services"
+	"github.com/majordomusio/commons/pkg/util"
+
 	"github.com/lnkk-ai/lnkk/pkg/shortener"
 	"github.com/lnkk-ai/lnkk/pkg/types"
-	"github.com/majordomusio/commons/pkg/util"
 )
 
 // ShortenEndpoint receives a URI to be shortened
@@ -26,7 +28,7 @@ func ShortenEndpoint(c *gin.Context) {
 		asset.URI = uri
 	}
 
-	standardJSONResponse(c, asset, err)
+	services.StandardJSONResponse(c, asset, err)
 }
 
 // RedirectEndpoint receives a URI to be shortened
